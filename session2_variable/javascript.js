6f209c5d2a04a68ad007deee89b64eda583f36e0
trankien386 <kien_iu_ai@yahoo.com>
2020-09-25 21:43:41 +0700

    Commit qua command-line

---
+++ session2_variable/javascript.js
@@ -0,0 +1,46 @@
+var a;
+		  let b;
+		  
+		  a=5;
+		  b=10;
+
+		  console.log(a);
+		  console.log(b);
+
+		  a=120;
+		  b=50;
+
+		  console.log(a);
+		  console.log(b);
+
+		  const pi=3.14;
+		  console.log(pi);
+
+		  var c;
+
+		  c=a+b;
+		  console.log(c);
+
+		  c=a-b;
+		  console.log(c);
+
+		  c=a*b;
+		  console.log(c);
+		  
+		  c=a/b;
+		  console.log(c);
+		
+		c=a%b; //chia lay so du
+		console.log(c);
+		  
+		let name = prompt("Nhap ten cua ban");
+		// let age = prompt("Nhap tuoi cua ban");
+		let birthyear = prompt("Ban sinh nam ?");
+
+		console.log("Xin chao " + name);
+		// console.log("Tuoi cua ban la " + age);
+		console.log("Ban sinh nam "+birthyear);
+		var currentyear = 2020;
+		var age;
+		age=currentyear-birthyear;
+		console.log("Tuoi cua ban la " +age);
